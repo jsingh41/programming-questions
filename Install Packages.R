cdd82bce1d1b78186662ef6d77616f08f29173e9
jsingh41 <hijuhii@gmail.com>
2016-09-27 15:57:33 -0700

    Adding Files related to R

---
+++ Install Packages.R
@@ -0,0 +1,26 @@
+# Hello all! This is an introductory script
+
+setwd("/Users/jsingh/Desktop/R")
+getwd()
+## Load packages in R
+# Use the following format:
+# install.packages("<the package's name>")
+
+install.packages("xlsx")
+install.packages("XLConnect")
+install.packages("dplyr")
+install.packages("stringr")
+install.packages("lubridate")
+install.packages("ggplot2")
+install.packages("shiny")
+install.packages("R markdown")
+install.packages("zoo")
+install.packages("xts")
+install.packages("quantmod")
+install.packages("data.table")
+install.packages("parallel")
+install.packages("XML")
+install.packages("jsonlite")
+install.packages("httr")
+
+install.packages("RODBC")
